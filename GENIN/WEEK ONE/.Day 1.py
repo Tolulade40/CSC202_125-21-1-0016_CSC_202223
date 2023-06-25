55a87e8d8aeeb843484ea749f41f2fdc5218870c
Tolulade40 <okunadetoluladesatar@gmail.com>
2023-06-25 21:18:51 +0100

    Python

---
+++ GENIN/WEEK ONE/.Day 1.py
@@ -0,0 +1,30 @@
+#printing, commenting, Debugging, String Manipulation and Variables
+print ("Hello World!")
+print ("Okunade Tolulade Satar")
+print ("This is my matric No: 125/21/1/0016")
+print ("Programme: Computer Science")
+print ("Course Code: CSC 202")
+print ("First Day")
+
+print ("print('Confirm')")
+
+#String Manipulation - 
+print ("Computer Sciences\nCyber Security\nSoftware Engineering")
+print ("Hello"+"Everyone")
+print ("Hello "+"Everyone")
+print ("Hello"+" Everyone")
+print ("Hello" + " "+"Everyone")
+
+print("Hello "+ input ("What is your name? "))
+print(len(input("What is your name? ")))
+
+#Python Variables
+name = input("what is your name? ")
+length = len(name)
+print(length)
+a = input ("a:")
+b= input("b:")
+print("a=" + a)
+print("b=" + b)
+input ("Whats your pet name?\n ")
+print("Your band name could be "+ input('What is the name of the city you grew up in? '))
