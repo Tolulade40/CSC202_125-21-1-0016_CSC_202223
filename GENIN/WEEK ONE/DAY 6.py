c1d44c4588e1b85d8cb0afc9ccd859528abdfefb
Tolulade40 <okunadetoluladesatar@gmail.com>
2023-06-25 21:14:53 +0100

    PYTHON

---
+++ GENIN/WEEK ONE/DAY 6.py
@@ -0,0 +1,48 @@
+def my_function():
+    print("Hello")
+    print("Bye")
+my_function()
+def turn_right():
+     turn_left():
+     turn_left():
+     turn_left():
+
+move()
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_left()
+move()
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_left()
+move()
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_left()
+for step in range(6):
+     jump()
+
+def turn_right():
+     turn_left()
+     turn_left()
+     turn_left()
+while not at_goal():
+     if right_is_clear():
+          turn_right()
+          move()
+        elif front_is_clear():
+          move()
+        else:
+          turn_left()
